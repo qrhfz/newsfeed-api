42dffd25e336073ebad75390e6492d94dc5e2b6a
q <mboh.dong@gmail.com>
2021-12-28 12:40:20 +0700

    refactor get rss

--- src/rssfeed/get-cnn-feed.ts
+++ src/rssfeed/get-cnn-feed.ts
@@ -1,19 +1,11 @@
-import Parser from "rss-parser";
 import { Article } from "../entities/article";
+import { getRssFeed } from "./get-rss-feed";
 
 
-const parser = new Parser();
-const url = "https://www.cnnindonesia.com/nasional/rss";
 
-export const getCnnFeed:()=>Promise<Article[]> = async ()=>{
-    const feed = await parser.parseURL(url);
-    let articles: Article[] = []
 
-    feed.items.forEach(item=>{
-        const {title, link, isoDate, contentSnippet, enclosure} = item;
-        let imageUrl = enclosure?.url;
-        const article: Article = new Article(title, link, new Date(isoDate??''),contentSnippet, imageUrl);
-        articles.push(article);
-    })
-    return articles;
+export const getCnnFeed: () => Promise<Article[]> = async () => {
+
+    const url = "https://www.cnnindonesia.com/nasional/rss";
+    return await getRssFeed(url);
 }
---
+++ src/rssfeed/get-rss-feed.ts
@@ -0,0 +1,18 @@
+import Parser from "rss-parser";
+import { Article } from "../entities/article";
+
+const parser = new Parser();
+
+
+export async function getRssFeed(url:string){
+    const feed = await parser.parseURL(url);
+    let articles: Article[] = []
+
+    feed.items.forEach(item=>{
+        const {title, link, isoDate, contentSnippet, enclosure} = item;
+        let imageUrl = enclosure?.url;
+        const article: Article = new Article(title, link, new Date(isoDate??''),contentSnippet, imageUrl);
+        articles.push(article);
+    })
+    return articles;
+}
