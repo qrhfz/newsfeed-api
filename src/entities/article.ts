41013440539ab790df4cb2bc60a79584c48b2031
q <mboh.dong@gmail.com>
2021-12-29 10:18:30 +0700

    migrate to cheerio

--- src/entities/article.ts
+++ src/entities/article.ts
@@ -1,19 +1,19 @@
 export class Article{
     constructor(
-        title?:string|undefined, 
-        link?:string|undefined, 
-        isoDate?:Date|undefined, 
-        content?:string|undefined,
-        imageUrl?:string|undefined){
+        title?:string, 
+        link?:string, 
+        isoDate?:Date, 
+        content?:string,
+        imageUrl?:string){
         this.title = title?? null;
         this.link = link?? null;
         this.isoDate = isoDate ?? null;
-        this.content = content ?? null;
+        this.snippet = content ?? null;
         this.imageUrl = imageUrl ?? null;
     }
     title: string|null;
     link: string|null;
     isoDate: Date|null;
-    content: string|null;
+    snippet: string|null;
     imageUrl: string|null;
 }
--- src/liputan6/index.ts
+++ src/liputan6/index.ts
@@ -2,6 +2,8 @@ import axios, { AxiosResponse } from "axios";
 
 import { JSDOM, VirtualConsole } from "jsdom";
 import { Article } from "../entities/article";
+import * as cheerio  from "cheerio"
+
 const virtualConsole = new VirtualConsole();
 const { getMetadata } = require('page-metadata-parser');
 
@@ -9,44 +11,50 @@ const { getMetadata } = require('page-metadata-parser');
 const frontPage: string = 'https://www.liputan6.com/'
 const headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0' }
 
-export async function getLiputan6FrontPage(): Promise<string> {
+export async function fetchHtmlPage(url: string): Promise<string|null>{
     try {
-        const { data } = await axios.get(frontPage, {
+        const { data } = await axios.get(url, {
             headers: headers
         });
         return JSON.stringify(data);
-    } catch (error) {
-        return JSON.stringify(error);
+    } catch (_) {
+        return null;
     }
+}
 
+export async function getLiputan6FrontPage(): Promise<string> {
+    const html = await fetchHtmlPage(frontPage)
+    if(html){
+        return html;
+    }else{
+        return '';
+    }
 }
 
 export function searchNewsLinks(html: string): string[] {
-    const dom = new JSDOM(html);
-    const links = dom.window.document.querySelectorAll("a");
-    const newsUrls: string[] = []
-    links.forEach(a => {
-        const url = a.href.valueOf();
-        const match = /https:\/\/www\.liputan6\.com\/.*\/read\/.*/.test(url)
-        if (match) {
-            newsUrls.push(a.href.valueOf());
+    // /https:\/\/www\.liputan6\.com\/.*\/read\/.*/.test(url)
+    const $ = cheerio.load(html)
+    const links: string[] = []
+    $('a').each((index, value)=>{
+        const url = $(value).attr('href')
+        if(!url) return;
+
+        const test = /https:\/\/www\.liputan6\.com\/.*\/read\/.*/.test(url)
+        if(test){
+            links.push(url)
         }
     })
-    return newsUrls;
+    return links;
 
 }
 
-export async function extractMetadata(url: string): Promise<Article | undefined> {
-    try {
-        const { data } = await axios.get(url, { headers: headers });
-        const html = JSON.stringify(data);
-        const dom = new JSDOM(html,{virtualConsole});
-        const doc = dom.window.document;
-        const {title, image, description} = getMetadata(doc, url);
-        return new Article(title, url, undefined, description, image);
-    } catch (error) {
-        return;
-    }
-
-
+export function extractMetadata(html: string): Article | undefined{
+    const $ = cheerio.load(html);
+    const url = $('meta[property="og:url"]').attr('content');
+    const title = $('meta[property="og:title"]').attr('content');
+    const description = $('meta[property="og:description"]').attr('content');
+    const image = $('meta[property="og:image"]').attr('content');
+    const dateString = $('meta[property="article:published_time"]').attr('content');
+    const date: Date|undefined = (dateString)?new Date(dateString):undefined
+    return new Article(title,url,date,description,image)
 }
